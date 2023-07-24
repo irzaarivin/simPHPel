0b7099437acad592163571e83fad61982fd6098c
Muhammad Irza Arivin Ngibadi <84498109+irzaarivin@users.noreply.github.com>
2023-07-24 16:21:56 +0700

    Update Config.php

--- brain/Config.php
+++ brain/Config.php
@@ -12,7 +12,7 @@ class Config {
     private static $db_server = "localhost";
     private static $db_user = "root";
     private static $db_password = "";
-    private static $db_name = "usk_latihan";
+    private static $db_name = "test";
 
     protected static function connect() {
         $conn = mysqli_connect(self::$db_server, self::$db_user, self::$db_password, self::$db_name);
